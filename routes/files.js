29d8186fc2c0eeb397baf849e80cf91c20121359
AnwarHossainSR <mahedianwar@gmail.com>
2022-01-28 23:04:51 +0600

    File upload api

--- routes/files.js
+++ routes/files.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const multer = require("multer");
 const path = require("path");
 const File = require("../model/file");
+const { v4: uuidv4 } = require("uuid");
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
@@ -17,11 +18,11 @@ let upload = multer({ storage, limits: { fileSize: 1000000 * 100 } }).single(
 ); //100mb
 
 router.post("/", (req, res) => {
-  if (!req.file) {
-    return res.status(422).send({ error: "All fields are required." });
-  }
   upload(req, res, async (err) => {
     //validation
+    if (!req.file) {
+      return res.status(422).send({ error: "All fields are required." });
+    }
     if (err) {
       return res.status(500).send({ error: err.message });
     }
